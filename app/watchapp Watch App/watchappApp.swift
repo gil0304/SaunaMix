4362b2a22fea645a4d887eaef5d491e75c4ada9f
gil <ryogo5.0304@gmail.com>
2025-02-11 20:35:25 +0900

    [add] app

---
+++ app/watchapp Watch App/watchappApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchappApp.swift
+//  watchapp Watch App
+//
+//  Created by 落合遼梧 on 2025/02/11.
+//
+
+import SwiftUI
+
+@main
+struct watchapp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
